9ab4bafb50a884a8fad2816694bb6219cb5ebe91
Enrique Aldana <aldana.sigona@gmail.com>
2019-09-30 14:42:23 +0300

    Define routes for the routelinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { RouterModule, Routes } from '@angular/router';
 
 /* Components */
-import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 
 const NAV_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch:'full', redirectTo: 'home' }
 
 ];
 
-export const APP_ROUTING = RouterModule.forRoot( NAV_ROUTES );
+export const APP_ROUTING = RouterModule.forRoot( NAV_ROUTES);
 /* Note:
   * add tag <router-outlet></router-outlet> in file component (~.component.html)
   * Need import and imports `app_routing`  in file module (~.module.ts) or ignore this message. */
